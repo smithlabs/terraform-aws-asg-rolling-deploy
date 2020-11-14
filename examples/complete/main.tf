dcd775fbc1067a83f95a918b4ff9f950654e0d63
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-14 08:14:28 -0700

    Grammar

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -36,7 +36,7 @@ module "asg" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# CREATE SECURITY RULE THAT ALLOWS ALL TRAFFIC IN ON THE SERVER PORT FROM ANYWHERE (TCP ONLY)
+# CREATE SECURITY RULE THAT ALLOWSS ALL TRAFFIC IN ON THE SERVER PORT FROM ANYWHERE (TCP ONLY)
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group_rule" "allow_server_http_inbound" {
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "allow_server_http_inbound" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# CREATE SECURITY RULE THAT ALLOW ALL TRAFFIC OUT OF THE SERVER TO ANYWHERE
+# CREATE SECURITY RULE THAT ALLOWS ALL TRAFFIC OUT OF THE SERVER TO ANYWHERE
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group_rule" "allow_server_all_outbound" {
